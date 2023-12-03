996864c568d20cefd13ef452ddf9ddd7b2817671
Fidel Ramos <f@fidelramos.net>
2023-12-03 19:30:36 +0000

    Use eza/exa for fzf dir preview if available

--- .config/fish/conf.d/fzf_config.fish
+++ .config/fish/conf.d/fzf_config.fish
@@ -1,3 +1,8 @@
 # https://github.com/PatrickF1/fzf.fish#configuration
 set fzf_diff_highlighter diff-so-fancy
 set fzf_fd_opts --hidden --exclude=.git
+if type -q eza
+    set fzf_preview_dir_cmd eza --all --color=always
+else if type -q exa
+    set fzf_preview_dir_cmd exa --all --color=always
+end
