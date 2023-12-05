110cc772d0aa9bcf3fbfe826b06b5b57bf6b4776
Fidel Ramos <f@fidelramos.net>
2023-12-05 15:35:52 +0000

    Port splitflac function to fish

---
+++ .config/fish/functions/splitflac.fish
@@ -0,0 +1,42 @@
+# Split a FLAC using a CUE file and tag the splitted tracks
+#
+function splitflac -a base
+    # drop trailing dot from base if user provides it, which is common when
+    # autocompleting in a shell
+    set base (string replace -r '\.$' '' $argv[1])
+    set cuefile $base.cue
+    set flacfile $base.flac
+
+    if test -z $base
+        echo "Usage: splitflac <basename>"
+        echo
+        echo "<basename> must be the common part to the .cue and .flac files."
+        echo
+        echo "For example when having two files Album Name.cue Album Name.flac"
+        echo "all it like this:"
+        echo
+        echo "  \$ splitflac \"Album Name\""
+        return 0
+    else if not test -f $cuefile
+        echo "$cuefile is not a file"
+        return 1
+    else if not test -f $flacfile
+        echo "$flacfile is not a file"
+        return 2
+    else if not type -q cuetag
+        echo "cuetag missing, usually found in cuetools package"
+        return 3
+    else if not type -q flac
+        echo "flac missing, usually found in flac package"
+        return 4
+    else if not type -q shnsplit
+        echo "shnsplit missing, usually found in shntool package"
+        return 5
+    end
+
+    shnsplit -f $cuefile -o flac $flacfile
+    or echo "Error splitting into multiple track files" && return 6
+
+    cuetag $cuefile split-track*
+    or echo "Error tagging splitted track files" && return 7
+end
