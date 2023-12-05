d85a22f1138d0d5957f9eb9892eb581071705066
Fidel Ramos <f@fidelramos.net>
2023-12-05 15:58:26 +0000

    Port nds function to fish

---
+++ .config/fish/functions/nds.fish
@@ -0,0 +1,27 @@
+# Get a shell inside a Podman container with NodeJS
+#
+# Current directory will be mounted in /app
+#
+# Will create a nodehome volume if it doesn't exist, and map it to /home/node
+# inside the Docker container. This enables persisting globally installed npm
+# packages.
+function nds
+    if not type -q podman
+        echo "Podman is required"
+    end
+    if not podman volume exists nodehome
+        echo "Creating volume nodehome"
+        podman volume create nodehome >/dev/null
+    end
+    podman run \
+        -it \
+        --rm \
+        --user=node \
+        --mount type=volume,src=nodehome,target=/home/node \
+        -v $PWD:/app:rw \
+        --workdir /app \
+        --entrypoint=/bin/bash \
+        --network host \
+        node:18-bullseye \
+        $argv
+end
