0bca4161c8feb6d70ca8786085e84c8d83a69b9b
Fidel Ramos <f@fidelramos.net>
2023-12-05 14:51:01 +0000

    Early check in cbr2cbz whether .cbz file exists

--- .config/fish/functions/cbr2cbz.fish
+++ .config/fish/functions/cbr2cbz.fish
@@ -1,14 +1,17 @@
 # transform a .cbr file into .cbz
 function cbr2cbz -a cbrfile
-    set -l cbrfile $argv[1]
+    set cbrfile $argv[1]
+    set cbzfile (string replace -f -r '.cbr$' .cbz $cbrfile)
+    or echo "$cbrfile doesn't seem to be a .cbr file" && return 2
+
     if test -z $cbrfile
         echo Usage: (status function) file.cbr
         return 1
-    else if not string match -q -r '.cbr$' $cbrfile
-        echo "$cbrfile doesn't seem to be a .cbr file"
-        return 2
     else if not test -f $cbrfile
         echo "$cbrfile not found"
+        return 2
+    else if test -e $cbzfile
+        echo "$cbzfile already exists"
         return 3
     else if not type -q unrar-free
         echo "unrar-free must be installed to extract the cbr file"
@@ -25,7 +28,6 @@ function cbr2cbz -a cbrfile
     unrar-free $cbrfile $tmpdir >/dev/null
     or echo "Error extracting $cbrfile into $tmpdir" && return 7
 
-    set -l cbzfile (string replace -f .cbr .cbz $cbrfile)
     and echo "Creating $cbzfile"
     zip -9rq $cbzfile "$tmpdir"/*
     or echo "Error creating $cbzfile" && return 8
