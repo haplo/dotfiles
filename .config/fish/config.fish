2c53c10acbedbf932c28c7489778fb7f56e7c5b8
Fidel Ramos <f@fidelramos.net>
2023-12-06 11:43:36 +0000

    Enable direnv in fish if available

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,11 @@
 set -U fish_greeting
 set -U fish_features ampersand-nobg-in-token,qmark-noglob,regex-easyesc,stderr-nocaret
 
+# enable direnv if available
+if type -q direnv
+    direnv hook fish | source
+end
+
 # source /etc/profile with bash
 if status is-login
     exec bash -c "test -e /etc/profile && source /etc/profile;\
