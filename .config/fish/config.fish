e277fae9334380ed80de3c9af10a15115375735a
Fidel Ramos <f@fidelramos.net>
2023-12-06 15:48:57 +0000

    Source /etc/profile when fish is used as login shell
    This fixes using fish as my default shell. :D

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,12 @@
 set -U fish_greeting
 set -U fish_features ampersand-nobg-in-token,qmark-noglob,regex-easyesc,stderr-nocaret
 
+# source /etc/profile in login shells
+if status is-login && test -e /etc/profile
+    fenv source /etc/profile
+end
+
 # enable direnv if available
 if type -q direnv
     direnv hook fish | source
-end
-
-# source /etc/profile with bash
-if status is-login
-    exec bash -c "test -e /etc/profile && source /etc/profile;\
-    exec fish"
 end
