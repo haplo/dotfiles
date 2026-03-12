9281b37e916f41e1bb3e26a34b7de67a0a3410a4
Fidel Ramos <contact.gyldd@8shield.net>
2026-03-12 22:22:00 +0000

    Support file deletion on update_fish_vendored_plugins.fish
    update_fish_vendored_plugins was only copying new files or updating
    existing ones, but not removing files that didn't exist anymore upstream.

--- .config/fish/functions/update_fish_vendored_plugins.fish
+++ .config/fish/functions/update_fish_vendored_plugins.fish
@@ -5,27 +5,31 @@
 function download_and_update
     set name $argv[1]
     set url $argv[2]
+
     echo "Updating $name from $url"
     set paths_to_copy $argv[3..]
     set tmp_dir (mktemp --directory)
     or echo "Error creating temporary directory" && return 1
     and echo "Extracting files to $tmp_dir"
+
     curl -L $url | tar xzf - --strip-components=1 -C $tmp_dir
+
     for path in $paths_to_copy
-        cp $tmp_dir/$path/* $HOME/.config/fish/vendor/$name/$path/
+        rm -r $HOME/.config/fish/vendor/$name/$path/
+        cp -r $tmp_dir/$path $HOME/.config/fish/vendor/$name/
     end
     rm -rf $tmp_dir
     echo
 end
 
 function update_fzf_fish
-    set FILES_TO_COPY completions/ conf.d/ functions/
-    download_and_update fzf-fish https://github.com/PatrickF1/fzf.fish/archive/refs/heads/main.tar.gz $FILES_TO_COPY
+    set DIRS_TO_COPY completions conf.d functions
+    download_and_update fzf-fish https://github.com/PatrickF1/fzf.fish/archive/refs/heads/main.tar.gz $DIRS_TO_COPY
 end
 
 function update_pure
-    set FILES_TO_COPY conf.d/ functions/
-    download_and_update pure https://github.com/pure-fish/pure/archive/refs/heads/master.tar.gz $FILES_TO_COPY
+    set DIRS_TO_COPY conf.d functions
+    download_and_update pure https://github.com/pure-fish/pure/archive/refs/heads/master.tar.gz $DIRS_TO_COPY
 end
 
 function update_fish_vendored_plugins
