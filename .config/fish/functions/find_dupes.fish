cf5d51f7e1ecd2b7e4db5abbfcb53d75ba3ba924
Fidel Ramos <contact.gyldd@8shield.net>
2026-03-16 22:26:48 +0000

    fish: fix find_dupes error when there are many files

--- .config/fish/functions/find_dupes.fish
+++ .config/fish/functions/find_dupes.fish
@@ -1,15 +1,16 @@
-function find_dupes -a path
-    set path $argv[1]
+function find_dupes
+    set -l dir $argv[1]
 
-    if test -z $path
+    if test -z "$dir"
         echo Usage: (status function)
         return 1
-    else if not test -d $path
-        echo "$path should be a directory"
+    else if not test -d "$dir"
+        echo "$dir should be a directory"
         return 2
     end
 
     # https://unix.stackexchange.com/questions/277697/whats-the-quickest-way-to-find-duplicated-files
     # maybe not the quickest, but simple and with no dependencies
-    find $path ! -empty -type f -print0 | xargs -0 md5sum | sort | uniq -w32 -dD
+    # xargs needs to batch the arguments to avoid argc getting over the 128 limit
+    find "$dir" ! -empty -type f -print0 | xargs -0 -n 125 md5sum | sort | uniq -w32 -dD
 end
